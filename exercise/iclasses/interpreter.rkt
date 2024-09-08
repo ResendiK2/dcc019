47bf3ec29423afa63e606ceb08cf03df3f4c25d9
Gabriel Resende <gabriel.arantesresende@gmail.com>
2024-09-08 14:56:41 -0300

    refactor: Update interpreter.rkt to add while and immutable logic

--- exercise/iclasses/interpreter.rkt
+++ exercise/iclasses/interpreter.rkt
@@ -14,33 +14,70 @@
     [(ast:dif e1 e2) (- (value-of e1 Δ) (value-of e2 Δ))]
     [(ast:zero? e) (zero? (value-of e Δ))]
     [(ast:not e) (not (value-of e Δ))]
-    [(ast:if e1 e2 e3) (if (value-of e1 Δ) (value-of e2 Δ) (value-of e3 Δ))]
-    [(ast:var v) (apply-env Δ v)] ; esta implementação só funciona para variáveis imutáveis
-    [(ast:let (ast:var x) e1 e2) (value-of e2 (extend-env x (value-of e1 Δ) Δ))]
-    [(ast:send e (ast:var mth) args) (display "send expression unimplemented")]
-    [(ast:super (ast:var c) args) (display "super expression unimplemented")]
-    [(ast:self) (display "self expression unimplemented")]
-    [(ast:new (ast:var c) args) (display "new expression unimplemented")]
-    [e (raise-user-error "unimplemented-construction: " e)]
-    ))
+    [(ast:var v) (apply-env Δ v)]
+    [(ast:if e1 e2 e3) 
+     (if (value-of e1 Δ) 
+         (value-of e2 Δ) 
+         (value-of e3 Δ))]
+    [(ast:let (ast:var x) e1 e2) 
+     (value-of e2 (extend-env x (value-of e1 Δ) Δ))]
+    [(ast:send e (ast:var mth) args) 
+     (display "send expression unimplemented")]
+    [(ast:super (ast:var c) args) 
+     (display "super expression unimplemented")]
+    [(ast:self) 
+     (display "self expression unimplemented")]
+    [(ast:new (ast:var c) args) 
+     (display "new expression unimplemented")]
+    [e (raise-user-error "unimplemented-construction: " e)]))
 
-; result-of :: Stmt -> Env -> State -> State
+; Função para processar declarações e comandos
 (define (result-of stmt Δ)
   (match stmt
-    [(ast:assign (ast:var x) e) (display "assignment unimplemented")]
-    [(ast:print e) (display (value-of e Δ))]
-    [(ast:return e) (value-of e Δ)]
-    [(ast:block stmts) (display "block unimplemented")]
-    [(ast:if-stmt e s1 s2) (display "if statment unimplemented")]
-    [(ast:while e s) (display "while unimplemented")]
-    [(ast:local-decl (ast:var x) s) (display "local var declaration unimplemented")]
-    [(ast:send e (ast:var mth) args) (display "command send unimplemented")]
-    [(ast:super (ast:var c) args) (display "command super unimplemented")]
-    [e (raise-user-error "unimplemented-construction: " e)]
-    ))
+    [(ast:assign (ast:var x) e)
+     (let ((new-val (value-of e Δ)))
+       (extend-env x new-val Δ))]
 
+    [(ast:print e)
+     (let ((val (value-of e Δ)))
+       (display val)
+       (newline)
+       Δ)]
+
+    [(ast:block stmts)
+     (foldl (lambda (s env) (result-of s env)) Δ stmts)]
+    [(ast:if-stmt e s1 s2)
+     (if (value-of e Δ)
+         (result-of s1 Δ)
+         (result-of s2 Δ))]
+
+    [(ast:return e)
+     (value-of e Δ)]
+
+    [(ast:while e s)
+     (while-loop e s Δ)]
+
+    [(ast:local-decl (ast:var x) s)
+     (let ((new-env (extend-env x 0 Δ)))
+       (result-of s new-env))]
+
+    [(ast:send e (ast:var mth) args) 
+     (display "send command unimplemented")]
+     
+    [(ast:super (ast:var c) args) 
+     (display "super command unimplemented")]
+    [e (raise-user-error "unimplemented-construction: " e)]))
+
+; Função para tratar loops while
+(define (while-loop e s Δ)
+  (let ((condition (value-of e Δ)))
+    (if condition
+        (let ((new-env (result-of s Δ)))
+          (while-loop e s new-env))
+        (displayln "Loop encerrado."))))
+
+; Função para avaliar um programa
 (define (value-of-program prog)
-  (empty-store)
   (match prog
     [(ast:prog decls stmt)
      (begin
